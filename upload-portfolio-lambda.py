c791c39294d35cc272387c6dd5c28b9e39cd13b4
Tapan Joshi <tapan.mjo5892@gmail.com>
2019-04-28 02:14:05 +1000

    python 2.7 updated

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,21 +4,29 @@ import StringIO
 import zipfile
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:019231189236:deploy-port')
 
-    build_bucket = s3.Bucket('portfoliobuild.tapanjo.info')
-    portfolio_bucket = s3.Bucket('portfolio.tapanjo.info')
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket = s3.Bucket('portfoliobuild.tapanjo.info')
+        portfolio_bucket = s3.Bucket('portfolio.tapanjo.info')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm)
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    print "Job Done"
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job Done"
+        topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully')
+    except:
+        topic.publish(Subject='Portfolio NOT Deployed', Message='Portfolio NOT deployed successfully')
+        raise
 
     return "Hello from Lambda"
             
