61715dd0afc93189af4eb42af8db72fedf98dabc
Tapan Joshi <tapan.mjo5892@gmail.com>
2019-05-08 00:19:29 +1000

    added work-test

---
+++ __tests__/test-example-work.js
@@ -0,0 +1,60 @@
+/**
+
+@jest-environment node
+*/
+
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWork, { ExampleWorkBubble } from '../js/example-work';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+const myWork = [
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example3.png",
+      'comment': `"Bengal cat" by roberto shabs is licensed under CC BY 2.0
+           https://www.flickr.com/photos/37287295@N00/2540855181`
+    }
+  },
+  {
+    'title': "Portfolio Boilerplate",
+    'image': {
+      'desc': "A Serverless Portfolio",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+  },
+];
+
+describe("ExampleWork component", () => {
+  let component = shallow(<ExampleWork work={myWork}/>);
+
+  it("Should be a 'section' element", () => {
+    expect(component.type()).toEqual('section');
+  });
+
+  it("Should contain as many children as work example", () => {
+    expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
+  });
+});
+
+describe("ExampleWorkBubble component", () => {
+  let component = shallow(<ExampleWorkBubble example={myWork[1]}/>);
+
+  let images = component.find("img");
+
+  it("Should contain single 'img element'", () => {
+    expect(images.length).toEqual(1);
+  });
+
+  it("Should have img src set correctly", () => {
+    expect(images.prop('src')).toEqual(myWork[1].image.src);
+  });
+
+});
