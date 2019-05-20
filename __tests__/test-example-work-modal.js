51e730efeefbbfe838c8fa0c7428c7aa69ea87f8
Tapan Joshi <tapan.mjo5892@gmail.com>
2019-05-20 20:07:38 +1000

    new changes to upload lamba

---
+++ __tests__/test-example-work-modal.js
@@ -0,0 +1,44 @@
+/**
+
+@jest-environment node
+*/
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWorkModal from '../js/example-work-modal';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+const myExample = {
+    'title': "Portfolio Boilerplate",
+    'href': "https://www.example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "A Serverless Portfolio",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+};
+
+describe("ExampleWorkModal component", () => {
+  let component = shallow(<ExampleWorkModal example = {myExample}
+      open={false}/>);
+  let openComponent = shallow(<ExampleWorkModal example = {myExample}
+      open={true}/>);
+
+  let anchors = component.find("a");
+
+  it("Should contain a single element 'a'", () => {
+    expect(anchors.length).toEqual(1);
+  });
+
+  it("Should link to our project", () => {
+    expect(anchors.prop('href')).toEqual(myExample.href);
+  });
+  it("Should have modal class set correctly", () => {
+    expect(component.find(".background--skyBlue").hasClass("modal--closed")).toBe(true);
+    expect(openComponent.find(".background--skyBlue").hasClass("modal--open")).toBe(true);
+  })
+});
--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,49 +1,55 @@
 import boto3
-from botocore.client import Config
 import StringIO
 import zipfile
+import mimetypes
 
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:019231189236:deploy-port')
 
-    location = {
-        "bucketName": 'portfoliobuild.tapanjo.info',
-        "objectKey": 'portfoliobuild.zip'
-        }
     try:
-        job = event.get('CodePipeline.job')
+        job = event.get("CodePipeline.job")
+
+        location = {
+            'bucketName': 'portfoliobuild.tapanjo.info',
+            'objectKey': 'portfoliobuild.zip'
+        }
 
         if job:
-            for artifact in job["data"]["inputArtifacts"]:
-                if artifact["name"] == "MyAppBuild":
-                    location = artifact["location"] ["s3Location"]
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == 'MyAppBuild':
+                    location = artifact['location']['s3Location']
 
-        print "Building Portfolio from " + str(location)
+        print "Building portfolio from " + str(location)
 
-        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        s3 = boto3.resource('s3')
 
         portfolio_bucket = s3.Bucket('portfolio.tapanjo.info')
-        build_bucket = s3.Bucket(location["bucketName"])
+        build_bucket = s3.Bucket(location['bucketName'])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-        print "Job Done"
-        topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully')
+
+        print "Job done!"
+        topic.publish(Subject="Portfolio Deployed",
+          Message="Portfolio deployed successfully!")
+
         if job:
             codepipeline = boto3.client('codepipeline')
-            codepipeline.put_job_success_result(jobId=job["id"])
+            codepipeline.put_job_success_result(jobId=job['id'])
 
     except:
-        topic.publish(Subject='Portfolio NOT Deployed', Message='Portfolio NOT deployed successfully')
+        topic.publish(Subject="Portfolio Deploy Failed",
+          Message="Portfolio was not deployed successfully.")
         raise
 
-    return "Hello from Lambda"
-            
+
+    return 'Hello from Lambda'
