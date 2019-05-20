2383f1b1f8b57942f69efceedca5c3d8bbe3d842
Tapan Joshi <tapan.mjo5892@gmail.com>
2019-05-20 21:47:07 +1000

    test

--- js/main.js
+++ js/main.js
@@ -5,8 +5,6 @@ import ExampleWork from './example-work';
 const myWork = [
   {
     'title': "Portfolio Boilerplate",
-    'href': "https://www.example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "A Serverless Portfolio",
       'src': "images/example2.png",
@@ -15,8 +13,6 @@ const myWork = [
   },
   {
     'title': "Work Example",
-    'href': "https://www.example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving cats",
       'src': "images/example3.png",
@@ -26,8 +22,6 @@ const myWork = [
   },
   {
     'title': "Work Example",
-    'href': "https://www.example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving code",
       'src': "images/example1.png",
