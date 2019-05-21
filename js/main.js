fc11909011b51fdede6a996bf3bb3051ab54c385
Tapan Joshi <tapan.mjo5892@gmail.com>
2019-05-21 16:18:02 +1000

    DB to aws S3

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ import ExampleWork from './example-work';
 
 const myWork = [
   {
-    'title': "Work Example",
+    'title': "Sync your Dropbox files to AWS S3",
     'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc': "Dropbox files to S3 for free.",
     'image': {
-      'desc': "A Serverless Portfolio",
+      'desc': "Sync Dropbox to AWS S3",
       'src': "images/example2.png",
       'comment': ""
     }
